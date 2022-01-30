1dbc7849a23596891b9f38527d12b6ee2c3822c9
adam026 <76121613+adam026@users.noreply.github.com>
2022-01-30 12:00:27 +0100

    Add project files.

---
+++ CATAGY_Lezerloveszet/JatekosLovese.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CATAGY_Lezerloveszet
+{
+    class JatekosLovese
+    {
+        static float CeltablaX { get; set; }
+        static float CeltablaY { get; set; }
+        public string Nev { get; set; }
+        public int Loves { get; set; }
+        public float LovesX { get; set; }
+        public float LovesY { get; set; }
+        public double Tavolsag => TKiszamol(LovesX, LovesY);
+        public double Pontszam => PontszamKiszamol(Tavolsag);
+
+        public JatekosLovese(string sor, int szamlalo)
+        {
+            if (szamlalo == 1)
+            {
+                var buffer = sor.Split(';');
+                CeltablaX = float.Parse(buffer[0]);
+                CeltablaY = float.Parse(buffer[1]);
+            }
+            else
+            {
+                var buff = sor.Split(';');
+                Nev = buff[0];
+                LovesX = float.Parse(buff[1]);
+                LovesY = float.Parse(buff[2]);
+                Loves = szamlalo - 1;
+            }
+            
+        }
+
+        private double TKiszamol(float kx, float ky)
+        {
+            var dx = CeltablaX - kx;
+            var dy = CeltablaY - ky;
+            return (Math.Sqrt((dx * dx) + (dy * dy)));
+        }
+
+        private double PontszamKiszamol(double tavolsag)
+        {
+            var eredmeny = 10 - tavolsag;
+            if (eredmeny <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return eredmeny;
+            }
+        }
+
+
+
+    }
+}
---
+++ CATAGY_Lezerloveszet/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CATAGY_Lezerloveszet
+{
+    internal class Program
+    {
+        static List<JatekosLovese> lovesek = new List<JatekosLovese>();
+        static Dictionary<string,int> jatekosokSzerint = new Dictionary<string, int>();
+        static Dictionary<string, double> atlagpontszamokSzerint = new Dictionary<string, double>();
+
+        static void Main(string[] args)
+        {
+            Beolvas();
+            F05();
+            F07();
+            F09();
+            F10();
+            F11();
+            F12();
+            F13();
+        }
+
+        private static void F13()
+        {
+            var nyertes = atlagpontszamokSzerint.OrderBy(x => x.Value).Last();
+            Console.WriteLine($"13. Feladat: A játék nyertese: {nyertes.Key}");
+        }
+
+        private static void F12()
+        {
+            Console.WriteLine("12. Feladat: Átlagpontszámok: ");
+            foreach (var jatekos in jatekosokSzerint)
+            {
+                var atlagPontszam = lovesek.Where(x => x.Nev == jatekos.Key).Average(y => y.Pontszam);
+                Console.WriteLine($"\t{jatekos.Key} - {atlagPontszam}");
+                if (!atlagpontszamokSzerint.ContainsKey(jatekos.Key))
+                {
+                    atlagpontszamokSzerint.Add(jatekos.Key, atlagPontszam);
+                }
+                else
+                {
+                    atlagpontszamokSzerint[jatekos.Key] += atlagPontszam;
+                }
+            }
+        }
+
+        private static void F11()
+        {
+            Console.WriteLine($"11. Feladat: Lövések száma: ");
+            foreach (var jatekos in jatekosokSzerint)
+            {
+                Console.WriteLine($"\t{jatekos.Key} - {jatekos.Value} db");
+            }
+        }
+
+        private static void F10()
+        {
+            for (int i = 1; i < lovesek.Count; i++) //Mivel a konstruktor miatt benne van a forrásállomány első sora is a listában!
+            {
+                if (!jatekosokSzerint.ContainsKey(lovesek[i].Nev))
+                {
+                    jatekosokSzerint.Add(lovesek[i].Nev, 1);
+                }
+                else
+                {
+                    jatekosokSzerint[lovesek[i].Nev] += 1;
+                }
+            }
+            Console.WriteLine($"10. Feladat: Játékosok száma: {jatekosokSzerint.Keys.Count}");
+        }
+
+        private static void F09()
+        {
+            var nullaPontosLovesek = lovesek.Where(x => x.Pontszam == 0).Count();
+            Console.WriteLine($"9. Feladat: Nulla pontos lövések száma: {nullaPontosLovesek - 1} db"); 
+            //Mivel a forrásállomány első sorát is számolja a konstruktor miatt
+        }
+
+        private static void F07()
+        {
+            var legpontosabb = lovesek.OrderBy(x => x.Tavolsag).First();
+            var hely = lovesek.IndexOf(legpontosabb);
+            Console.WriteLine($"7. Feladat: Legpontosabb lövés: \n\t{hely}. {legpontosabb.Nev} x={legpontosabb.LovesX}; y={legpontosabb.LovesY}; távolság: {legpontosabb.Tavolsag}");
+        }
+
+        private static void F05()
+        {
+            Console.WriteLine($"5. Feladat: Lövések száma: {lovesek.Count} db");
+        }
+
+        private static void Beolvas()
+        {
+            using (var sr = new StreamReader(@"..\..\RES\lovesek.txt", Encoding.UTF8))
+            {
+                var szamlalo = 1;
+
+                while (!sr.EndOfStream)
+                {
+                    lovesek.Add(new JatekosLovese(sr.ReadLine(), szamlalo));
+                    szamlalo++;
+                }
+
+            }
+        }
+    }
+}
